f151bfea690b586235edb11711b6e6c989dfae56
Mohammad <mohmmad.salamat84@gmail.com>
2021-11-12 12:17:43 +0200

    composer dump-autoload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\helperController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/get-split', [helperController::class, "getSplitName"]);
+Route::get('/get-split', [helperController::class, 'getSplitName']);
